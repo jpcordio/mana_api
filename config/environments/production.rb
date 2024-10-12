c6d2fdfa0005c67dce3b983bd2b8961b0fb9f3ec
Joao Paulo Cordio <joao.cordio@gmail.com>
2024-10-12 20:13:58 +0100

    fix deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
 
   # Email
   config.action_mailer.default_url_options = {
-    host: 'mana-api-92b61d2011eb.herokuapp.com'
+    host: 'mana-api.herokuapp.com'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
